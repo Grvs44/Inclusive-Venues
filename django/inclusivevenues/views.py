2fb99a72faf37e80f640ea5c506a5e417ea102a9
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-01-22 14:23:16 +0000

    Add model ViewSets

--- django/inclusivevenues/views.py
+++ django/inclusivevenues/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# Adapted from https://www.django-rest-framework.org/api-guide/viewsets/#modelviewset
+from rest_framework.viewsets import ModelViewSet
+from . import models, serializers
 
-# Create your views here.
+
+class VenueCategoryViewSet(ModelViewSet):
+    queryset = models.VenueCategory.objects
+    serializer_class = serializers.VenueCategorySerializer
+
+
+class VenueSubcategoryViewSet(ModelViewSet):
+    queryset = models.VenueSubcategory.objects
+    serializer_class = serializers.VenueSubcategorySerializer
+
+
+class VenueViewSet(ModelViewSet):
+    queryset = models.Venue.objects
+    serializer_class = serializers.VenueSerializer
+
+
+class ReviewViewSet(ModelViewSet):
+    queryset = models.Review.objects
+    serializer_class = serializers.ReviewSerializer
+
+
+class RatingCategoryViewSet(ModelViewSet):
+    queryset = models.RatingCategory.objects
+    serializer_class = serializers.RatingCategorySerializer
+
+
+class RatingViewSet(ModelViewSet):
+    queryset = models.Rating.objects
+    serializer_class = serializers.RatingSerializer
