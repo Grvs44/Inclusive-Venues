b39b61bd1504e20b0d73e422d736b0d1b2a52280
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-01-22 21:43:25 +0000

    Add routing

---
+++ django/inclusivevenues/urls.py
@@ -0,0 +1,19 @@
+'''
+Routing for Inclusive Venues Django app
+Router documentation: https://www.django-rest-framework.org/api-guide/routers/
+'''
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'venuecat', views.VenueCategoryViewSet)
+router.register(r'venuesub', views.VenueSubcategoryViewSet)
+router.register(r'venue', views.VenueViewSet)
+router.register(r'review', views.ReviewViewSet)
+router.register(r'ratingcat', views.RatingCategoryViewSet)
+router.register(r'rating', views.RatingViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
