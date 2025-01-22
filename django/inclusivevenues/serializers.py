0b861e6e6bcaa03f711655ff2c249e67d32f9918
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-01-22 12:17:01 +0000

    Add serializers

---
+++ django/inclusivevenues/serializers.py
@@ -0,0 +1,47 @@
+# Adapted from https://www.django-rest-framework.org/api-guide/serializers/#modelserializer
+from rest_framework.serializers import ModelSerializer
+from . import models
+
+
+class VenueCategorySerializer(ModelSerializer):
+    class Meta:
+        model = models.VenueCategory
+        fields = ['id', 'name']
+
+
+class VenueSubcategorySerializer(ModelSerializer):
+    class Meta:
+        model = models.VenueSubcategory
+        fields = ['id', 'name', 'category']
+
+
+class VenueSerializer(ModelSerializer):
+    class Meta:
+        model = models.Venue
+        fields = [
+            'id',
+            'name',
+            'description',
+            'longitude',
+            'latitude',
+            'address',
+            'subcategory',
+        ]
+
+
+class ReviewSerializer(ModelSerializer):
+    class Meta:
+        model = models.VenueCategory
+        fields = ['id', 'author', 'venue', 'body']
+
+
+class RatingCategorySerializer(ModelSerializer):
+    class Meta:
+        model = models.VenueCategory
+        fields = ['id', 'name', 'description']
+
+
+class RatingSerializer(ModelSerializer):
+    class Meta:
+        model = models.VenueCategory
+        fields = ['id', 'category', 'review', 'value']
