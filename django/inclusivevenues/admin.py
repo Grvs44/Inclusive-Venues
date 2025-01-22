227a2ea5c3d34e7b2b4ddb495e467cb4d572d52d
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-01-22 11:57:55 +0000

    Register models with Django admin

--- django/inclusivevenues/admin.py
+++ django/inclusivevenues/admin.py
@@ -1,3 +1,37 @@
+# Adapted from https://docs.djangoproject.com/en/5.1/ref/contrib/admin/
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class ModelAdmin(admin.ModelAdmin):
+    list_per_page = 10
+
+
+@admin.register(models.VenueCategory)
+class VenueCategoryAdmin(ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.VenueSubCategory)
+class VenueSubCategoryAdmin(ModelAdmin):
+    list_display = ['category__name', 'name']
+
+
+@admin.register(models.Venue)
+class VenueAdmin(ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Review)
+class ReviewAdmin(ModelAdmin):
+    list_display = ['author__name', 'venue__name']
+
+
+@admin.register(models.RatingCategory)
+class RatingCategoryAdmin(ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Rating)
+class RatingAdmin(ModelAdmin):
+    list_display = ['review__venue__name', 'category__name', 'value']
