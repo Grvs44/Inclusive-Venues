4fa63aa16d411ae528afee65b2c95d318e16b66c
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-01-24 12:57:14 +0000

    Change title component

--- react/src/containers/TopBar.tsx
+++ react/src/containers/TopBar.tsx
@@ -31,7 +31,7 @@ export default function TopBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
             {title}
           </Typography>
         </Toolbar>
