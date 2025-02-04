3b66654012a15cec3d158886410321b7d69d89f3
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-02-04 12:04:56 +0000

    Use venue card instead of a popup Fixes #9

--- react/src/components/VenueMarker.tsx
+++ react/src/components/VenueMarker.tsx
@@ -1,31 +1,20 @@
 import React from 'react'
-import Typography from '@mui/material/Typography'
-import { AzureMapHtmlMarker, AzureMapPopup } from 'react-azure-maps'
+import { AzureMapHtmlMarker } from 'react-azure-maps'
 import { ListVenue } from '../redux/types'
 
 export type VenueMarkerProps = {
   venue: ListVenue
+  onClick: (venue: ListVenue) => void
 }
 
-export default function VenueMarker({ venue }: VenueMarkerProps) {
-  const [open, setOpen] = React.useState<boolean>(false)
-  const position = [venue.longitude, venue.latitude]
-
+export default function VenueMarker({ venue, onClick }: VenueMarkerProps) {
   return (
-    <>
-      <AzureMapHtmlMarker
-        events={[{ eventName: 'click', callback: () => setOpen(true) }]}
-        options={{
-          text: venue.score?.toFixed(1),
-          position,
-        }}
-      />
-      <AzureMapPopup
-        isVisible={open}
-        options={{ position }}
-        events={[{ eventName: 'close', callback: () => setOpen(false) }]}
-        popupContent={<Typography component="p">{venue.name}</Typography>}
-      />
-    </>
+    <AzureMapHtmlMarker
+      events={[{ eventName: 'click', callback: () => onClick(venue) }]}
+      options={{
+        text: venue.score?.toFixed(1),
+        position: [venue.longitude, venue.latitude],
+      }}
+    />
   )
 }
--- react/src/containers/MapResultsView.tsx
+++ react/src/containers/MapResultsView.tsx
@@ -1,27 +1,53 @@
 import React from 'react'
 import 'azure-maps-control/dist/atlas.min.css'
-import { CircularProgress } from '@mui/material'
+import { Box, Card, CircularProgress, Paper, Typography } from '@mui/material'
 import { useAzureMaps } from 'react-azure-maps'
+import Link from '../components/Link'
 import LocationMarker from '../components/LocationMarker'
 import Map from '../components/Map'
+import StarBox from '../components/StarBox'
 import VenueMarker from '../components/VenueMarker'
 import { useGetVenuesQuery } from '../redux/apiSlice'
+import { ListVenue } from '../redux/types'
 
 export default function MapResultsView() {
   const { mapRef, isMapReady } = useAzureMaps()
   const { data, isLoading } = useGetVenuesQuery({})
+  const [selectedVenue, setSelectedVenue] = React.useState<
+    ListVenue | undefined
+  >(undefined)
+
+  const onMarkerClick = (newVenue: ListVenue) =>
+    setSelectedVenue((oldVenue) =>
+      newVenue.id === oldVenue?.id ? undefined : newVenue,
+    )
 
   return (
-    <div style={{ height: '300px' }}>
-      <Map>
-        <>
-          <LocationMarker />
-          {data?.results.map((venue) => (
-            <VenueMarker key={venue.id} venue={venue} />
-          ))}
-        </>
-      </Map>
-      {isMapReady || isLoading ? null : <CircularProgress />}
-    </div>
+    <Paper>
+      <Box style={{ height: '300px' }}>
+        <Map>
+          <>
+            <LocationMarker />
+            {data?.results.map((venue) => (
+              <VenueMarker
+                key={venue.id}
+                venue={venue}
+                onClick={onMarkerClick}
+              />
+            ))}
+          </>
+        </Map>
+        {isMapReady || isLoading ? null : <CircularProgress />}
+      </Box>
+      {selectedVenue ? (
+        <Card>
+          <Typography component="h2" variant="h4">
+            {selectedVenue.name}
+          </Typography>
+          <StarBox value={selectedVenue.score} />
+          <Link to={selectedVenue.id.toString()}>View details</Link>
+        </Card>
+      ) : null}
+    </Paper>
   )
 }
