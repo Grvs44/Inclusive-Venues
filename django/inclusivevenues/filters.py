e08cc361946aad5cef25462b809fd1433178cf53
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-02-27 12:44:40 +0000

    Start LocationFilter

--- django/inclusivevenues/filters.py
+++ django/inclusivevenues/filters.py
@@ -1,3 +1,5 @@
+from decimal import Decimal, InvalidOperation
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import BaseFilterBackend
 from django.db.models import Q
 
@@ -11,7 +13,7 @@ def split_params(arg: str) -> list[int]:
     return result
 
 
-class VenueFilter(BaseFilterBackend):
+class CategoryFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         categories = split_params(request.GET.get('category', ''))
         subcategories = split_params(request.GET.get('subcategory', ''))
@@ -22,3 +24,41 @@ class VenueFilter(BaseFilterBackend):
         if categories:
             return queryset.filter(subcategory__category__in=categories)
         return queryset.filter(subcategory__in=subcategories)
+
+
+def get_location(location: str) -> tuple[Decimal, Decimal] | None:
+    if location == '':
+        return None
+    lat, lon = location.split(',', 2)
+    try:
+        lat_d = Decimal(lat)
+    except InvalidOperation as e:
+        raise ValidationError('Latitude must be a number') from e
+    try:
+        lon_d = Decimal(lon)
+    except InvalidOperation as e:
+        raise ValidationError('Longitude must be a number') from e
+    return lat_d, lon_d
+
+
+class LocationFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        location = get_location(request.GET.get('location', ''))
+        if location is None:
+            return queryset
+        radius = 2
+        lat, lon = location
+        km_lat = Decimal(0.00902) * radius
+        km_lon = Decimal(0.00898) * radius
+        lat_max = lat + km_lat
+        lat_min = lat - km_lat
+        lon_max = lon + km_lon
+        lon_min = lon - km_lon
+        print(lat_max,lon_max)
+        print(lat_min,lon_min)
+        return queryset.filter(
+            latitude__lt=lat_max,
+            latitude__gt=lat_min,
+            longitude__lt=lon_max,
+            longitude__gt=lon_min,
+        )
