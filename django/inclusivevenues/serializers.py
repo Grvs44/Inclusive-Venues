1aba5cdd79244f17cd6bc675039a61cd4ed84aa7
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-06 12:04:19 +0000

    Add back venue latitude and longitude properties

--- django/inclusivevenues/serializers.py
+++ django/inclusivevenues/serializers.py
@@ -46,11 +46,14 @@ class VenueSerializer(ModelSerializer):
 
 class VenueListSerializer(ModelSerializer):
     distance = DecimalField(max_digits=5, decimal_places=1, read_only=True)
+
     class Meta:
         model = models.Venue
         fields = [
             'id',
             'name',
+            'longitude',
+            'latitude',
             'distance',
             'subcategory',
             'score',
