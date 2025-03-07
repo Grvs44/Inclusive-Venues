17e3856965e56e921e9d6d27203a0aa4abd60853
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-07 17:18:49 +0000

    Move tests to subpackage

---
+++ django/inclusivevenues/tests/test_models.py
@@ -0,0 +1,68 @@
+'''Tests for inclusivevenues.models'''
+# pylint:disable=no-member
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from django.test import TransactionTestCase
+from .. import models
+
+
+class ModelTestCase(TransactionTestCase):
+    '''TransactionTestCase for inclusivevenues.models'''
+
+    def setUp(self):
+        category = models.VenueCategory.objects.create(name='category1')
+        self.subcategory = models.VenueSubcategory.objects.create(
+            name='subcategory1', category=category)
+        self.rating_category = models.RatingCategory.objects.create(
+            name='category1', description='rating category 1')
+
+    def test_unique_constraints(self):
+        '''Test the unique constraints prevent duplicate reviews and ratings from being created'''
+        user = User.objects.create_user('constraint_user')
+        venue = models.Venue.objects.create(
+            name='venue1', added_by=user, longitude=0, latitude=0, subcategory=self.subcategory)
+        review = models.Review.objects.create(
+            author=user, venue=venue)
+        models.Rating.objects.create(
+            review=review, category=self.rating_category, value=3)
+        self.assertRaises(
+            IntegrityError,
+            models.Rating.objects.create,
+            review=review, category=self.rating_category, value=4
+        )
+        self.assertRaises(
+            IntegrityError,
+            models.Review.objects.create,
+            author=user, venue=venue
+        )
+
+    def test_venue_calculate_score(self):
+        '''Test the Venue score is calculated correctly'''
+        rating_category1 = models.RatingCategory.objects.create(
+            name='venue_score1', description='rating category 1')
+        rating_category2 = models.RatingCategory.objects.create(
+            name='venue_score2', description='rating category 2')
+        user1 = User.objects.create_user('venue_user1')
+        user2 = User.objects.create_user('venue_user2')
+        venue = models.Venue.objects.create(
+            name='University', added_by=user1, longitude=50.937665, latitude=-1.395655, subcategory=self.subcategory)
+
+        self.assertEqual(venue.score, None, 'Initial score')
+        venue.update_score()
+        venue.refresh_from_db()
+        self.assertEqual(venue.score, None, 'Score with no reviews')
+
+        review1 = models.Review.objects.create(author=user1, venue=venue)
+        models.Rating.objects.create(
+            review=review1, category=rating_category1, value=2)
+        models.Rating.objects.create(
+            review=review1, category=rating_category2, value=4)
+        review2 = models.Review.objects.create(author=user2, venue=venue)
+        models.Rating.objects.create(
+            review=review2, category=rating_category1, value=5)
+        models.Rating.objects.create(
+            review=review2, category=rating_category2, value=3)
+
+        venue.update_score()
+        venue.refresh_from_db()
+        self.assertEqual(venue.score, 3.5, 'Score with ratings')
