ec9b95983b471ea1cf62ab2febada6bc8c8cd669
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-13 10:21:00 +0000

    Open ReviewDialog when edit button is clicked

--- react/src/pages/ReviewListPage.tsx
+++ react/src/pages/ReviewListPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import { useDispatch } from 'react-redux'
+import ReviewDialog from '../containers/ReviewDialog'
 import ReviewList from '../containers/ReviewList'
 import { useGetReviewsQuery } from '../redux/apiSlice'
 import { setTitle } from '../redux/titleSlice'
@@ -9,11 +10,14 @@ import { Review } from '../redux/types'
 
 export default function ReviewListPage() {
   const dispatch = useDispatch()
+  const [reviewOpen, setReviewOpen] = React.useState<boolean>(false)
+  const [venueId, setVenueId] = React.useState<number | undefined>(undefined)
   const [page, setPage] = React.useState<number>(1)
   const { data, isLoading } = useGetReviewsQuery({ page })
 
   const onEdit = (review: Review) => {
-    console.log(review)
+    setVenueId(review.venue)
+    setReviewOpen(true)
   }
 
   React.useEffect(() => {
@@ -28,6 +32,11 @@ export default function ReviewListPage() {
           Load more
         </Button>
       ) : null}
+      <ReviewDialog
+        open={reviewOpen}
+        onClose={() => setReviewOpen(false)}
+        venueId={venueId}
+      />
     </Container>
   )
 }
