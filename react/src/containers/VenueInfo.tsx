b31167cc66c1c73893ea63e74e6b6ba1a390b82d
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-17 15:05:25 +0000

    Remove links from venue breadcrumbs #69

--- react/src/containers/VenueInfo.tsx
+++ react/src/containers/VenueInfo.tsx
@@ -3,7 +3,6 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import Breadcrumbs from '@mui/material/Breadcrumbs'
 import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
-import Link from '../components/Link'
 import StarBox from '../components/StarBox'
 import { Venue } from '../redux/types'
 
@@ -18,11 +17,9 @@ export default function VenueInfo({ venue }: { venue: Venue }) {
         {venue.name}
       </Typography>
       <Breadcrumbs separator={<NavigateNextIcon fontSize="inherit" />}>
-        <Link to="/venue">Venues</Link>
-        <Link to={`/venue?cat=${category.data.id}`}>{category.data.name}</Link>
-        <Link to={`/venue?sub=${subcategory.data.id}`}>
-          {subcategory.data.name}
-        </Link>
+        <>Venues</>
+        <>{category.data.name}</>
+        <>{subcategory.data.name}</>
       </Breadcrumbs>
       <StarBox value={venue.score} />
       <Typography>{venue.description}</Typography>
