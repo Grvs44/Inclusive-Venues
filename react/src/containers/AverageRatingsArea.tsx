d542bcc1fe7dce733c0cd8bdf17b3fccd1265ab0
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-24 11:23:44 +0000

    Show RatingCategory names

--- react/src/containers/AverageRatingsArea.tsx
+++ react/src/containers/AverageRatingsArea.tsx
@@ -4,14 +4,18 @@ import Grid from '@mui/material/Grid2'
 import Skeleton from '@mui/material/Skeleton'
 import Typography from '@mui/material/Typography'
 import StarBox from '../components/StarBox'
-import { useGetVenueReviewAggregationQuery } from '../redux/apiSlice'
+import {
+  useGetRatingCategoriesQuery,
+  useGetVenueReviewAggregationQuery,
+} from '../redux/apiSlice'
 
 export type AverageRatingsBoxProps = {
   id: number
 }
 
 export default function AverageRatingsArea(props: AverageRatingsBoxProps) {
-  const { data, isFetching } = useGetVenueReviewAggregationQuery(props.id)
+  const categories = useGetRatingCategoriesQuery()
+  const ratings = useGetVenueReviewAggregationQuery(props.id)
 
   return (
     <Card>
@@ -24,14 +28,16 @@ export default function AverageRatingsArea(props: AverageRatingsBoxProps) {
         rowSpacing={0}
         columns={{ xs: 4, sm: 8, md: 12, lg: 16, xl: 20 }}
       >
-        {data?.map((rating) => (
+        {ratings.data?.map((rating) => (
           <Grid key={rating.category} size={4} color="GrayText">
-            <Typography>{rating.category}</Typography>
+            <Typography>
+              {categories.data?.find((c) => c.id == rating.category)?.name}
+            </Typography>
             <StarBox value={rating.value} sx={{ fontSize: 'inherit' }} />
           </Grid>
         ))}
       </Grid>
-      {isFetching ? <Skeleton /> : null}
+      {categories.isFetching || ratings.isFetching ? <Skeleton /> : null}
     </Card>
   )
 }
