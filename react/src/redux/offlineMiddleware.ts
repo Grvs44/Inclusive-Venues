8cae0f42eec0a235877bfadadddf853d01ee143c
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-28 13:07:51 +0000

    Remove toast from middleware

--- react/src/redux/offlineMiddleware.ts
+++ react/src/redux/offlineMiddleware.ts
@@ -1,7 +1,6 @@
 // Adapted from https://redux-toolkit.js.org/rtk-query/usage/error-handling#handling-errors-at-a-macro-level
 import { isRejectedWithValue } from '@reduxjs/toolkit'
 import type { Middleware, MiddlewareAPI } from '@reduxjs/toolkit'
-import toast from 'react-hot-toast'
 
 const offlineMessage =
   'You are currently offline, please check your connection and try again'
@@ -13,7 +12,6 @@ const offlineMiddleware: Middleware =
       (action.payload as any)?.status == 'FETCH_ERROR'
     ) {
       ;(action.payload as any).error = offlineMessage
-      toast.error(offlineMessage, { duration: 5000, id: 'o' })
     }
 
     return next(action)
