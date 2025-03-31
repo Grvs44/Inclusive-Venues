b483bbce82ad7a5ae2880556e303a5d8aefa0cbb
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-31 13:06:32 +0100

    Apply initial settings

--- react/src/providers/FilterProvider.tsx
+++ react/src/providers/FilterProvider.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import type { VenueSubcategory } from '../redux/types'
+import toast from 'react-hot-toast'
+import { useSelector } from 'react-redux'
+import { getLocationErrorMessage } from '../components/utils'
+import type { State, VenueSubcategory } from '../redux/types'
 
 export type Filters = {
   subcategories: VenueSubcategory[]
@@ -21,11 +24,38 @@ const FilterContext = React.createContext<Filters | undefined>(undefined)
 export const useFilters = () => React.useContext(FilterContext)
 
 export default function FilterProvider(props: FilterProviderProps) {
+  const settings = useSelector((state: State) => state.settings)
   const [subcategories, setSubcategories] = React.useState<VenueSubcategory[]>(
     [],
   )
   const [location, setLocation] = React.useState<string>('')
   const [radius, setRadius] = React.useState<string>('')
+
+  // Apply initial settings
+  React.useEffect(() => {
+    if (settings.defaultLocation) {
+      setLocation(
+        `${settings.defaultLocation[0]},${settings.defaultLocation[1]}`,
+      )
+    }
+    if (settings.autoLocation && navigator.geolocation) {
+      const id = toast.loading('Fetching your location...')
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          setLocation(
+            `${coords.latitude.toFixed(6)},${coords.longitude.toFixed(6)}`,
+          )
+          toast.dismiss(id)
+        },
+        (error) => {
+          toast.dismiss(id)
+          toast.error(
+            'Error auto-detecting location:\n' + getLocationErrorMessage(error),
+          )
+        },
+      )
+    }
+  }, [])
 
   const addSubcategories: Filters['addSubcategories'] = (subs) =>
     setSubcategories((subcategories) => subcategories.concat(subs))
