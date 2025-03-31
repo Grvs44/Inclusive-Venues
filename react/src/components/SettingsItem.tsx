8d4f41fe4cd58bf175c5d2c8caba8c0ea29ea812
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-03-31 12:42:58 +0100

    Fix children layout

--- react/src/components/SettingsItem.tsx
+++ react/src/components/SettingsItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Box from '@mui/material/Box'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
@@ -15,18 +16,20 @@ export type SettingsItemProps = {
 export default function SettingsItem(props: SettingsItemProps) {
   return (
     <ListItem>
-      <FormControlLabel
-        control={
-          <Switch
-            checked={props.checked}
-            onChange={(event) => props.onChange(event.target.checked)}
-          />
-        }
-        label={
-          <ListItemText primary={props.primary} secondary={props.secondary} />
-        }
-      />
-      {props.checked ? props.children : undefined}
+      <Box>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={props.checked}
+              onChange={(event) => props.onChange(event.target.checked)}
+            />
+          }
+          label={
+            <ListItemText primary={props.primary} secondary={props.secondary} />
+          }
+        />
+        {props.checked ? props.children : undefined}
+      </Box>
     </ListItem>
   )
 }
