058e14c1f66541647a987df5bae8229cfe96f231
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-04-07 17:14:53 +0100

    Add app verbose_name

--- django/inclusivevenues/apps.py
+++ django/inclusivevenues/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class InclusivevenuesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inclusivevenues'
+    verbose_name = 'Inclusive Venues'
