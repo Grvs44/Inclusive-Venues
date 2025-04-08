c59e3fe9979bb6110b584df6df8434060d2783c2
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-04-08 10:24:34 +0100

    Click venue map preview to open map Fixes #169

--- react/src/containers/VenueLocation.tsx
+++ react/src/containers/VenueLocation.tsx
@@ -4,18 +4,31 @@ import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+import toast from 'react-hot-toast'
 import { Venue } from '../redux/types'
 import { openMaps } from '../redux/utils'
 
 export default function VenueLocation({ venue }: { venue: Venue }) {
+  const click = () => {
+    toast.loading('Opening Map app...', { duration: 4000 })
+    openMaps(venue)
+  }
+
   return (
     <Card sx={{ width: '50%' }}>
       <Stack>
         {venue.map ? (
-          <img src={venue.map} loading="lazy" alt="Map preview" />
+          <img
+            src={venue.map}
+            loading="lazy"
+            alt="Map preview"
+            title="Click to open in Maps"
+            onClick={click}
+            style={{ cursor: 'pointer' }}
+          />
         ) : null}
         {venue.address ? <Typography>{venue.address}</Typography> : null}
-        <Button variant="contained" onClick={() => openMaps(venue)}>
+        <Button variant="contained" onClick={click}>
           Open in Maps
           <OpenInNewIcon fontSize="inherit" />
         </Button>
