63fea264a3f3e1eac9f2a4770bba7810ac9849d9
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-04-19 19:39:33 +0100

    Use any URL for API baseUrl

--- react/src/redux/apiSlice.ts
+++ react/src/redux/apiSlice.ts
@@ -60,7 +60,7 @@ const forceRefetch = <T>({
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.BASE_URL + import.meta.env.VITE_API_URL,
+    baseUrl: import.meta.env.VITE_API_URL,
     // prepareHeaders adapted from https://redux-toolkit.js.org/rtk-query/api/fetchBaseQuery#setting-default-headers-on-requests
     prepareHeaders(headers) {
       const csrfToken = Cookies.get('csrftoken')
