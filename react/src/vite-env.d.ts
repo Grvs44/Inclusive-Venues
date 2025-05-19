18a022f2c9529510dc85d18641e4e1c90d062cb3
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-05-19 15:46:18 +0100

    Make Maps optional in React app Fixes #188

--- react/src/vite-env.d.ts
+++ react/src/vite-env.d.ts
@@ -3,7 +3,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
-  readonly VITE_AZURE_MAP_KEY: string
+  readonly VITE_AZURE_MAP_KEY?: string
   readonly VITE_DEFAULT_LATITUDE?: string
   readonly VITE_DEFAULT_LONGITUDE?: string
   readonly VITE_VERSION?: string
